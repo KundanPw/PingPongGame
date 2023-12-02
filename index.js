f76effbd5b66c77e4ec0e32fe6c876423409351b
Kundan <kk9797307@gmail.com>
2023-12-02 10:37:59 +0530

    Added paddle movement of mouse

--- index.js
+++ index.js
@@ -23,9 +23,16 @@ document.addEventListener("DOMContentLoaded", () => {
         // if(ballX > 700-20 || ballX <= 0) dx *= -1;
         // if(ballY > 400-20 || ballY <= 0) dy *= -1;
 
+        /**
+         * ballX < paddle.offsetLeft + paddle.offsetWidth -> if left(w.r.t table) of ball < right(w.r.t table) of the paddle
+         * ballY > paddle.offsetTop -> if  top(w.r.t table) of ball > top(w.r.t table) of paddle
+         *  ballY - ball.offsetHeight < paddle.offsetTop + paddle.offsetHeight ->
+         * ballY - ball.offsetHeight -> button of the ball
+         * paddle.offsetTop + paddle.offsetHeigh -> button of paddle
+         */
         if(ballX < paddle.offsetLeft + paddle.offsetWidth && 
             ballY > paddle.offsetTop && 
-            ballY - ball - ball.offsetHeight < paddle.offsetTop + paddle.offsetHeight
+            ballY + ball.offsetHeight < paddle.offsetTop + paddle.offsetHeight
             ) {
             dx *= -1;
         }
@@ -52,6 +59,15 @@ document.addEventListener("DOMContentLoaded", () => {
             //down arrow
             paddleY += dPy;
         }
+        paddle.style.top = `${paddleY}px`;
+    });
+
+    document.addEventListener("mousemove" , (event) => {
+        let mouseDistanceFromTop = event.clientY; // this is the distance of the mosue point from the top of the screen
+        let distanceOfTableFromTop = table.offsetTop;
+        let mousePointControl = mouseDistanceFromTop - distanceOfTableFromTop - paddle.offsetHeight/2;
+        paddleY = mousePointControl
+        if(paddleY <= 0 || paddleY > table.offsetHeight - paddle.offsetHeight ) return; // if button of the paddle touches button of the table
         paddle.style.top = `${paddleY}px`;
     })
 });
